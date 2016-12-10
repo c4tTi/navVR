bc16bf9c82a777c96806e15ffa50d7f94b865156
c4tTi <groux@gmx.net>
2016-12-10 15:20:40 +0100

    new sample project

---
+++ projects/sampleProjects/mordentral-vrexppluginexample-3b63c607efdf/Intermediate/Build/Win64/UE4Editor/Development/VRExpPluginExample/PCH.VRExpPluginExample.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\vrStuff\mordentral-vrexppluginexample-3b63c607efdf\Source\VRExpPluginExample\VRExpPluginExample.h"
---
+++ projects/sampleProjects/mordentral-vrexppluginexample-3b63c607efdf/Intermediate/Build/Win64/UE4Editor/Development/VRExpPluginExample/VRExpPluginExample.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_VREXPPLUGINEXAMPLE_H__
+#define __AUTO_VREXPPLUGINEXAMPLE_H__
+//Last Write: 09/12/2016 12:53:33 +00:00
+#include "C:\vrStuff\mordentral-vrexppluginexample-3b63c607efdf\Source\VRExpPluginExample\VRExpPluginExample.h"
+#endif//__AUTO_VREXPPLUGINEXAMPLE_H__
