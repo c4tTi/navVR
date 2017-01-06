b38e132ed4244c2fb4eb6e3a22579d4b53b93f3a
Dominic Bär <dominic.baer@students.fhnw.ch>
2017-01-06 04:42:43 +0100

    RENAME to NavVRSpace

--- projects/NavVRReal/Intermediate/Build/Win64/UE4Editor/Development/VRExpPluginExample/PCH.VRExpPluginExample.h.cpp
+++ projects/NavVRReal/Intermediate/Build/Win64/UE4Editor/Development/VRExpPluginExample/PCH.VRExpPluginExample.h.cpp
@@ -1,1 +1,1 @@
-#include "D:\navVR\projects\NavVRReal\Source\VRExpPluginExample\VRExpPluginExample.h"
+#include "C:\navVR\projects\NavVRReal\Source\VRExpPluginExample\VRExpPluginExample.h"
--- projects/NavVRReal/Intermediate/Build/Win64/UE4Editor/Development/VRExpPluginExample/VRExpPluginExample.h
+++ projects/NavVRReal/Intermediate/Build/Win64/UE4Editor/Development/VRExpPluginExample/VRExpPluginExample.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_VREXPPLUGINEXAMPLE_H__
 #define __AUTO_VREXPPLUGINEXAMPLE_H__
-//Last Write: 05.01.2017 14:26:48 +00:00
-#include "D:\navVR\projects\NavVRReal\Source\VRExpPluginExample\VRExpPluginExample.h"
+//Last Write: 05.01.2017 16:10:05 +00:00
+#include "C:\navVR\projects\NavVRReal\Source\VRExpPluginExample\VRExpPluginExample.h"
 #endif//__AUTO_VREXPPLUGINEXAMPLE_H__
