1bd757c0d501ebbb460123cf98e9026a37583ddd
dombaer <dominic.baer@students.fhnw.ch>
2017-01-08 20:57:56 +0100

    hmm

--- projects/NavVRReal/Intermediate/Build/Win64/UE4Editor/Development/VRExpPluginExample/VRExpPluginExample.h
+++ projects/NavVRReal/Intermediate/Build/Win64/UE4Editor/Development/VRExpPluginExample/VRExpPluginExample.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_VREXPPLUGINEXAMPLE_H__
 #define __AUTO_VREXPPLUGINEXAMPLE_H__
-//Last Write: 05.01.2017 16:10:05 +00:00
+//Last Write: 08.01.2017 17:22:41 +00:00
 #include "C:\navVR\projects\NavVRReal\Source\VRExpPluginExample\VRExpPluginExample.h"
 #endif//__AUTO_VREXPPLUGINEXAMPLE_H__
